7410c3ad2e8d818779b45388bf34089da35e7076
Olga Chekh <helga.chekh@gmail.com>
2023-10-26 16:49:51 -0500

    Update vail_systems_coding_assignment.py
    clarify if/elif statement for readiness

--- vail_systems_coding_assignment.py
+++ vail_systems_coding_assignment.py
@@ -2,8 +2,9 @@ arr = [1,2,3,4,5,6,7]
 position = 6
 
 def array_rotation(arr, position):
-    if position < 0: raise Exception("Position argument can't be below zero.")
-    if position > len(arr): position = position - len(arr)
+    if position < 0: 
+        raise Exception("Position argument can't be below zero.")
+    elif position > len(arr): position = position - len(arr)
 
     while position > 0:
         arr.append(arr[0])
